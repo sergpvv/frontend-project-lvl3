6959a6edebcea0436f67e2bf60adda4f3d3b318d
sergpvv <serg.pvv@gmail.com>
2022-04-09 00:37:08 +0000

    wham bam

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
   devtool: 'inline-source-map',
-  /* devServer: {
-    // static: './dist',
-    // hot: true,
-    host: '10.0.2.15',
-    port: 8080,
-  }, */
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
   module: {
     rules: [
       {
@@ -16,7 +16,14 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
